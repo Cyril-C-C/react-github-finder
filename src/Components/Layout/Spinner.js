b78d8ed4ad55d5c984e10f0f62255faa754e17b0
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2021-08-04 15:43:42 +0100

    Progressive Changes

---
+++ src/Components/Layout/Spinner.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Spinner from './Spinner.gif';
+
+const Spinner = () => <Fragment>
+      <img
+        src='Spinner'
+        alt='Loading...'
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
+    </Fragment>
+
+
+export default Spinner;
