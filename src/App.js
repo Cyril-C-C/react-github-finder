8e43dcae85f0f0fb5ef9666562187d9b423d9e99
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2021-08-04 12:34:15 +0100

    Progressive Changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Layout/Navbar';
 import Users from './Components/User/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
 
   render() {
     return (
       <div className='App'>
         <Navbar />
-          <div className='container'>
-            <Users />
-          </div>
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
--- src/Components/User/UserItem.js
+++ src/Components/User/UserItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const UserItem = ({ user: { login, avartar_url, html_url } }) => {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className='card text-center'>
       <img
-        src={avartar_url}
+        src={avatar_url}
         alt=''
         className='round-img'
         style={{ width: '3.75rem' }}
